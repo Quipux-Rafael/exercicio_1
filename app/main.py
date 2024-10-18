5e64ac7b38c02bcc58df1cdfdace10a80caa5581
Rafael Gomes da Silva <rafael.gomes@quipuxbrasil.com.br>
2024-10-18 19:22:59 -0300

    fastAPI e Dockerfile

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import ast
+from datetime import datetime
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/lista-ordenada/")
+def lista_ordenada(lista_no_ordenada):
+    lista_no_ordenada = lista = ast.literal_eval(lista_no_ordenada)
+    lista_ordenada = sorted(lista_no_ordenada)
+    hora_atual = datetime.now().strftime("%d/%m/%Y %H:%m:%S")
+
+    return {
+        "hora_sistema": hora_atual,
+        "lista_ordenada": lista_ordenada
+    }
+
+
+@app.get("/healthcheck")
+def healthcheck():
+    return "Ok"
