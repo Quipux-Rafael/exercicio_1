e442751de6ad7cd53b415f6fea34c4b03636522a
Rafael Gomes da Silva <rafael.gomes@quipux.com.br>
2024-10-18 20:53:04 -0300

    Adicionado excecao para caso n seja enviado uma lista

--- exercicio_docker/main.py
+++ exercicio_docker/main.py
@@ -9,8 +9,11 @@ app = FastAPI()
 
 @app.get("/lista-ordenada/")
 def lista_ordenada(lista_no_ordenada):
-    lista_no_ordenada = ast.literal_eval(lista_no_ordenada)
-    lista_ordenada = sorted(lista_no_ordenada)
+    try:
+        lista_no_ordenada = ast.literal_eval(lista_no_ordenada)
+        lista_ordenada = sorted(lista_no_ordenada)
+    except:
+        lista_ordenada = []
     hora_atual = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     return {
